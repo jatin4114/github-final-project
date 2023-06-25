8c6d0d78cc3bfa858d63ad662ef373e679224364
jatin4114 <104613594+jatin4114@users.noreply.github.com>
2023-06-26 00:28:04 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # jatin4114
    # Input:
    # p, principal amount
    # t, time period in years
